c318ebf2099ec95fa635cac0f0ff3d21cb31d9b0
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-10-22 01:03:45 +0300

    Finish task 01

--- 01_cli_interactive_sort/index.js
+++ 01_cli_interactive_sort/index.js
@@ -1,30 +1,41 @@
-const readline = require('readline');
+/*
+test word -- cherry 9 raspberry 17 23 11 56 strawberry 2 34 6 lemon 45 85 8 quince 12 tangerine 78 4 67 orange grape honeydew date 90 62 33 apple kiwi fox 27 strawberry 85
+             -123 45.67 -0.5 0 123  A1 b2 C3 d4 E5 f6 G7 h8 I9 j0 + - ?
 
-const rl = readline.createInterface({
+*/
+const readlineConsole = require('readline');
+
+const cli = readlineConsole.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function getUserInput() {
-    rl.question('Enter a few words or numbers separated by a space (or type "exit" to quit): ', (input) => {
+
+let values = [];
+
+const getUserInput = () => {
+    cli.question('Enter a few words or numbers separated by a space (or type "exit" to quit): ', (input) => {
         if (input.toLowerCase() === 'exit') {
-            rl.close();
+            cli.close();
         } else {
-            processInput(input);
+            values = processInput(input); // Оновлюємо значення
+            showOperationMenu();
         }
     });
 }
 
-function processInput(input) {
-    const values = input.split(' ').map((value) => {
-        if (!isNaN(value)) {
+const processInput = (input) => {
+    return input.split(' ').map((value) => {
+        if (typeof value === 'string' && /^-?\d*\.?\d+$/.test(value)) {
             return parseFloat(value);
         } else {
             return value;
         }
     });
+}
 
-    rl.question(`Choose an operation:
+const showOperationMenu = () => {
+    cli.question(`Choose an operation:
 1. Sort words alphabetically
 2. Show numbers from lesser to greater
 3. Show numbers from bigger to smaller
@@ -34,22 +45,32 @@ function processInput(input) {
 Enter the number corresponding to your choice: `, (choice) => {
         switch (parseInt(choice)) {
             case 1:
-                values.sort();
+                const words = values.filter((value) => typeof value === 'string');
+                words.sort();
+                console.log('Result: ', words.join(' '));
                 break;
             case 2:
-                values.sort((a, b) => a - b);
+                const numbersLesser = values.filter((value) => typeof value === 'number');
+                numbersLesser.sort((a, b) => a - b);
+                console.log('Result: ', numbersLesser.join(' '));
                 break;
             case 3:
-                values.sort((a, b) => b - a);
+                const numbersGreater = values.filter((value) => typeof value === 'number');
+                numbersGreater.sort((a, b) => b - a);
+                console.log('Result: ', numbersGreater.join(' '));
                 break;
             case 4:
-                values.sort((a, b) => a.toString().length - b.toString().length);
+                const wordsByLength = values.filter((value) => typeof value === 'string');
+                wordsByLength.sort((a, b) => a.length - b.length);
+                console.log('Result: ', wordsByLength.join(' '));
                 break;
             case 5:
-               // values = values.filter((value, index, self) => self.indexOf(value) === index && typeof value === 'string');
+                const uniqueWords = values.filter((value, index, self) => self.indexOf(value) === index && typeof value === 'string');
+                console.log('Result: ', uniqueWords.join(' '));
                 break;
             case 6:
-               // values = values.filter((value, index, self) => self.indexOf(value) === index);
+                const uniqueValues = values.filter((value, index, self) => self.indexOf(value) === index);
+                console.log('Result: ', uniqueValues.join(' '));
                 break;
             default:
                 console.log('Invalid choice. Please enter a valid choice (1-6).');
@@ -57,7 +78,6 @@ Enter the number corresponding to your choice: `, (choice) => {
                 return;
         }
 
-        console.log('Result: ', values.join(' '));
         getUserInput();
     });
 }
