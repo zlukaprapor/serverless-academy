9e904bb45113c0e77485a47944c7fb6dd3874929
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-10-22 01:11:02 +0300

    refactor code

--- 01_cli_interactive_sort/index.js
+++ 01_cli_interactive_sort/index.js
@@ -6,80 +6,87 @@ test word -- cherry 9 raspberry 17 23 11 56 strawberry 2 34 6 lemon 45 85 8 quin
 const readlineConsole = require('readline');
 
 const cli = readlineConsole.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
-
 
 let values = [];
 
 const getUserInput = () => {
-    cli.question('Enter a few words or numbers separated by a space (or type "exit" to quit): ', (input) => {
-        if (input.toLowerCase() === 'exit') {
-            cli.close();
-        } else {
-            values = processInput(input); // Оновлюємо значення
-            showOperationMenu();
-        }
-    });
-}
+  cli.question(
+    'Enter a few words or numbers separated by a space (or type "exit" to quit): ',
+    input => {
+      if (input.toLowerCase() === 'exit') {
+        cli.close();
+      } else {
+        values = processInput(input);
+        showOperationMenu();
+      }
+    },
+  );
+};
 
-const processInput = (input) => {
-    return input.split(' ').map((value) => {
-        if (typeof value === 'string' && /^-?\d*\.?\d+$/.test(value)) {
-            return parseFloat(value);
-        } else {
-            return value;
-        }
-    });
-}
+const processInput = input => {
+  return input.split(' ').map(value => {
+    if (typeof value === 'string' && /^-?\d*\.?\d+$/.test(value)) {
+      return parseFloat(value);
+    } else {
+      return value;
+    }
+  });
+};
 
 const showOperationMenu = () => {
-    cli.question(`Choose an operation:
+  cli.question(
+    `Choose an operation:
 1. Sort words alphabetically
 2. Show numbers from lesser to greater
 3. Show numbers from bigger to smaller
 4. Display words in ascending order by the number of letters
 5. Show only unique words
 6. Display only unique values
-Enter the number corresponding to your choice: `, (choice) => {
-        switch (parseInt(choice)) {
-            case 1:
-                const words = values.filter((value) => typeof value === 'string');
-                words.sort();
-                console.log('Result: ', words.join(' '));
-                break;
-            case 2:
-                const numbersLesser = values.filter((value) => typeof value === 'number');
-                numbersLesser.sort((a, b) => a - b);
-                console.log('Result: ', numbersLesser.join(' '));
-                break;
-            case 3:
-                const numbersGreater = values.filter((value) => typeof value === 'number');
-                numbersGreater.sort((a, b) => b - a);
-                console.log('Result: ', numbersGreater.join(' '));
-                break;
-            case 4:
-                const wordsByLength = values.filter((value) => typeof value === 'string');
-                wordsByLength.sort((a, b) => a.length - b.length);
-                console.log('Result: ', wordsByLength.join(' '));
-                break;
-            case 5:
-                const uniqueWords = values.filter((value, index, self) => self.indexOf(value) === index && typeof value === 'string');
-                console.log('Result: ', uniqueWords.join(' '));
-                break;
-            case 6:
-                const uniqueValues = values.filter((value, index, self) => self.indexOf(value) === index);
-                console.log('Result: ', uniqueValues.join(' '));
-                break;
-            default:
-                console.log('Invalid choice. Please enter a valid choice (1-6).');
-                processInput(input);
-                return;
-        }
+Enter the number corresponding to your choice: `,
+    choice => {
+      switch (parseInt(choice)) {
+        case 1:
+          const words = values.filter(value => typeof value === 'string');
+          words.sort();
+          console.log('Result: ', words.join(' '));
+          break;
+        case 2:
+          const numbersLesser = values.filter(value => typeof value === 'number');
+          numbersLesser.sort((a, b) => a - b);
+          console.log('Result: ', numbersLesser.join(' '));
+          break;
+        case 3:
+          const numbersGreater = values.filter(value => typeof value === 'number');
+          numbersGreater.sort((a, b) => b - a);
+          console.log('Result: ', numbersGreater.join(' '));
+          break;
+        case 4:
+          const wordsByLength = values.filter(value => typeof value === 'string');
+          wordsByLength.sort((a, b) => a.length - b.length);
+          console.log('Result: ', wordsByLength.join(' '));
+          break;
+        case 5:
+          const uniqueWords = values.filter(
+            (value, index, self) => self.indexOf(value) === index && typeof value === 'string',
+          );
+          console.log('Result: ', uniqueWords.join(' '));
+          break;
+        case 6:
+          const uniqueValues = values.filter((value, index, self) => self.indexOf(value) === index);
+          console.log('Result: ', uniqueValues.join(' '));
+          break;
+        default:
+          console.log('Invalid choice. Please enter a valid choice (1-6).');
+          processInput(input);
+          return;
+      }
 
-        getUserInput();
-    });
-}
+      getUserInput();
+    },
+  );
+};
 
 getUserInput();
