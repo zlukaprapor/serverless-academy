e03207994a81c5a64117a89071d02c66e11c8d7a
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-10-25 18:00:45 +0300

    task 6 finish

--- 06_instagram_giveaway/index.js
+++ 06_instagram_giveaway/index.js
@@ -8,6 +8,7 @@ function readWordsFromFile(filename) {
 }
 
 // знаходження унікальних імен користувачів, які зустрічаються хоча б в одному з файлів.
+//використовуються алгоритми пошук унікальних значень
 function uniqueValues() {
   const uniqueUsernames = new Set();
 
@@ -19,20 +20,18 @@ function uniqueValues() {
 }
 
 // знаходження імен користувачів, які зустрічаються в усіх 20 файлах.
+// використовуються алгоритми об'єднання та перетин множин
 function existInAllFiles() {
   const commonUsernames = new Set();
 
-  // Ініціалізуємо commonUsernames множиною імен з першого файлу
   const wordsInFirstFile = readWordsFromFile('out0.txt');
   wordsInFirstFile.forEach(word => commonUsernames.add(word));
 
   for (let i = 1; i < 20; i++) {
     const words = readWordsFromFile(`out${i}.txt`);
 
-    // Створюємо множину для імен з поточного файлу
     const currentFileUsernames = new Set(words);
 
-    // Використовуємо перетин множин для збереження тільки спільних імен
     commonUsernames.forEach(username => {
       if (!currentFileUsernames.has(username)) {
         commonUsernames.delete(username);
@@ -44,6 +43,7 @@ function existInAllFiles() {
 }
 
 // знаходження імен користувачів, які зустрічаються хоча б в 10 файлах.
+// використовуються алгоритми пошуку
 function existInAtleastTen() {
   const usernameCountMap = new Map();
 
@@ -59,7 +59,6 @@ function existInAtleastTen() {
     });
   }
 
-  // Фільтруємо лише імена, які зустрічаються принаймні 10 разів
   let countAtLeastTen = 0;
   for (const count of usernameCountMap.values()) {
     if (count >= 10) {
@@ -74,14 +73,50 @@ function existInAtleastTen() {
 const start = performance.now();
 console.log('Unique Usernames:', uniqueValues());
 const end = performance.now();
-console.log('Elapsed time (uniqueValues):', end - start, 'ms');
+const elapsedMilliseconds1 = end - start;
+const elapsedMinutes1 = Math.floor(elapsedMilliseconds1 / 60000); // Визначаємо кількість хвилин
+const remainingSeconds1 = (elapsedMilliseconds1 % 60000) / 1000; // Визначаємо кількість секунд
+const elapsedMillisecondsFormatted1 = (elapsedMilliseconds1 % 1000).toFixed(0); // Визначаємо мілісекунди
+console.log(
+  'Elapsed time (uniqueValues):',
+  elapsedMinutes1,
+  'minute',
+  Math.floor(remainingSeconds1),
+  'second',
+  elapsedMillisecondsFormatted1,
+  'millisecond',
+);
 
 const start2 = performance.now();
 console.log('Usernames in All Files:', existInAllFiles());
 const end2 = performance.now();
-console.log('Elapsed time (existInAllFiles):', end2 - start2, 'ms');
+const elapsedMilliseconds2 = end2 - start2;
+const elapsedMinutes2 = Math.floor(elapsedMilliseconds2 / 60000);
+const remainingSeconds2 = (elapsedMilliseconds2 % 60000) / 1000;
+const elapsedMillisecondsFormatted2 = (elapsedMilliseconds2 % 1000).toFixed(0);
+console.log(
+  'Elapsed time (existInAllFiles):',
+  elapsedMinutes2,
+  'minute',
+  Math.floor(remainingSeconds2),
+  'second',
+  elapsedMillisecondsFormatted2,
+  'millisecond',
+);
 
 const start3 = performance.now();
 console.log('Usernames in At Least 10 Files:', existInAtleastTen());
 const end3 = performance.now();
-console.log('Elapsed time (existInAtleastTen):', end3 - start3, 'ms');
+const elapsedMilliseconds3 = end3 - start3;
+const elapsedMinutes3 = Math.floor(elapsedMilliseconds3 / 60000);
+const remainingSeconds3 = (elapsedMilliseconds3 % 60000) / 1000;
+const elapsedMillisecondsFormatted3 = (elapsedMilliseconds3 % 1000).toFixed(0);
+console.log(
+  'Elapsed time (existInAtleastTen):',
+  elapsedMinutes3,
+  'minute',
+  Math.floor(remainingSeconds3),
+  'second',
+  elapsedMillisecondsFormatted3,
+  'millisecond',
+);
--- 06_instagram_giveaway/readme.md
+++ 06_instagram_giveaway/readme.md
@@ -1,44 +1,11 @@
-6. INSTAGRAM GIVEAWAY
-   The goal of this task is to learn methods of sorting and selecting values. All you need to implement it is vanilla JavaScript without any external dependencies.
+Test result
+"C:\Program Files\nodejs\node.exe" C:\WebstormProjects\serverless-academy\06_instagram_giveaway\index.js
 
-What makes the task interesting is that at one time we used a similar algorithm to find the likes of users in 10 posts in a client-side giveaway. It was necessary to find out who actually liked all ten posts.
+Unique Usernames: 129240
+Elapsed time (uniqueValues): 0 minute 2 second 629 millisecond
+Usernames in All Files: 441
+Elapsed time (existInAllFiles): 0 minute 1 second 846 millisecond
+Usernames in At Least 10 Files: 108345
+Elapsed time (existInAtleastTen): 0 minute 2 second 570 millisecond
 
-Overview
-You have 20 text files (download), each with 100,000 word combinations. The total is 2 million word combinations. They were generated from two batches of 400 random words each, so they are repeatedly repeated in all or some of the files.
-
-You need:
-
-Determine how many unique usernames there are in all the specified files (occurring at least once in any of the files);
-Determine how many usernames occur in all 20 files;
-Find out how many usernames occur in at least 10 files.
-Requirements
-Your program should have 3 different functions that return an integer. For example:
-
-uniqueValues(); // returns 1234
-existInAllFiles(); // returns 42
-existInAtleastTen(); // returns 50
-⚠️ NOTE: Make sure you measure the performance of your program.
-
-If it takes more than a few seconds to process, you are doing something wrong. Also, report the elapsed time in the program’s output.
- ІНСТАГРАМ РОЗІГРА
-   Метою цього завдання є вивчення методів сортування та вибору значень. Все, що вам потрібно для його реалізації, — це ванільний JavaScript без будь-яких зовнішніх залежностей.
-
-Що робить завдання цікавим, так це те, що свого часу ми використовували подібний алгоритм, щоб знайти лайки користувачів у 10 публікаціях у клієнтській роздачі. Треба було з'ясувати, кому насправді лайкнули всі десять постів.
-
-Огляд
-У вас є 20 текстових файлів (завантажити), кожен із яких містить 100 000 словосполучень. Всього 2 мільйони словосполучень. Вони були згенеровані з двох груп по 400 випадкових слів кожна, тому вони багаторазово повторюються у всіх або деяких файлах.
-
-Тобі потрібно:
-
-Визначте, скільки унікальних імен користувачів є у всіх зазначених файлах (зустрічаються принаймні один раз у будь-якому з файлів);
-Визначте, скільки імен користувачів зустрічається у всіх 20 файлах;
-Дізнайтеся, скільки імен користувачів зустрічається принаймні в 10 файлах.
-Вимоги
-Ваша програма повинна мати 3 різні функції, які повертають ціле число. Наприклад:
-
-унікальні значення (); // повертає 1234
-existInAllFiles(); // повертає 42
-existInAtleastTen(); // повертає 50
-⚠️ ПРИМІТКА. Обов’язково вимірюйте продуктивність вашої програми.
-
-Якщо обробка займає більше кількох секунд, ви робите щось не так. Крім того, повідомте час, що минув, у вихідних даних програми.
+Process finished with exit code 0
